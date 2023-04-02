0d317cc594cec7e71d6930e9e088f346626aad54
Rodelole <89358290+Rrodelo54@users.noreply.github.com>
2023-04-02 11:01:28 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const menu = document.getElementById("head");
+
+let contet = document.createElement("header");
+contet.classList.add("header");
+//si se puede agregar clases a etiquetas desde aqui
+contet.innerHTML = `
+  <div class="logo">
+  <img src="/img/api.png">
+  Wed Api Free</div>
+  <input type="checkbox" id="toggle">
+  <label for="toggle"><img src="/img/menu-logo.png" class="men" alt=""></label>
+  <nav class="navegation">
+    <ul>
+    <li><a href="/index.html">INICIO</a></li>
+    <li><a href="/apis.html">APIs</a></li>
+    </ul>
+    </nav>
+    `;
+menu.append(contet);
+//
+//
+const cards = document.getElementById("container_cards");
+
+apis.forEach((p_apis) => {
+    let conten = document.createElement("div")
+    conten.classList.add("cards");
+    conten.innerHTML = `
+    <img src="${p_apis.img}">
+    <h3>${p_apis.name}</h3>
+    <span>Tipo: ${p_apis.tipo}</span>
+    <a href="${p_apis.link}">VER API</a>
+    `;
+
+    container_cards.append(conten)
+
+});
---
+++ json.js
@@ -0,0 +1,72 @@
+const apis = [
+    /*
+    {
+        name:"",
+        img: "",
+        tipo:"publica",
+        link:"",
+    },
+
+   */
+    {
+        name:"Twitter API",
+        img: "https://aeroleads.com/blog/wp-content/uploads/2019/10/595944-636247477374032053-16x9.jpg",
+        tipo:"privada",
+        link:"https://developer.twitter.com/en/docs",
+    },
+    {
+        name:"Instagram API",
+        img: "https://blog.hubspot.com/hs-fs/hubfs/instagram-feed-api-example.jpeg?width=1200&name=instagram-feed-api-example.jpeg",
+        tipo:"publica",
+        link:" https://www.instagram.com/developer/",
+    },
+    {
+        name:"Facebook API",
+        img: "https://www.gizlogic.com/wp-content/uploads/2018/08/Facebook-API-2.jpg",
+        tipo:"privada",
+        link:"https://developers.facebook.com/docs/apis-and-sdks",
+    },
+    {
+        name:"api de Marvel",
+        img:"https://edteam-media.s3.amazonaws.com/blogs/original/e83d6108-d484-47dd-8bf5-cf44632e4a48.png",
+        tipo:"publica",
+        link:"https://developer.marvel.com/",
+    },
+    {
+        name:"PokéAPI",
+        img:"https://edteam-media.s3.amazonaws.com/blogs/original/192ad64b-4b13-44c4-a0a0-23e4af446c06.png",
+        tipo:"publica",
+        link:"https://pokeapi.co/",
+    },
+    {
+        name:"COVID Tracking",
+        img: "https://edteam-media.s3.amazonaws.com/blogs/original/a9613ed1-9f35-4e57-a500-938480e853b4.png",
+        tipo:"publica",
+        link:"https://covidtracking.com/data/",
+    },
+    {
+        name:"Nomics",
+        img: "https://edteam-media.s3.amazonaws.com/blogs/original/e81c7ec7-3a29-4d8a-a5ff-ec5971077dab.png",
+        tipo:"publica",
+        link:"https://nomics.com/",
+    },
+    {
+        name:"OpenWeather APIs",
+        img: "https://edteam-media.s3.amazonaws.com/blogs/original/f381046b-b8b4-426f-bbd6-5a599b9c7329.png",
+        tipo:"publica",
+        link:"https://openweathermap.org/api",
+    },
+    {
+        name:"JSON placeholder",
+        img:"https://sergioiglesias.net/images/pildoras-web/jsonplaceholder.png",
+        tipo:"publica",
+        link:"https://jsonplaceholder.typicode.com/",
+    },
+    {
+        name:"Twitter API",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmGZoe3-DfK9SFZFPc0eMXAQTFoj902kc92sa4YpAj16TRlPijqJc1LTzdafLkUF672Hs&usqp=CAU",
+        tipo:"privada",
+        link:"https://developer.twitter.com/en/docs",
+    },
+
+]
